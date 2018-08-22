1d5b8a78b3fabd23a71d63f7537ffbbd0267bb4b
Matt Welke <mattwelke@gmail.com>
2018-08-21 22:06:59 -0400

    Improve push and download scripts.

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+PARAM_SKIP_DL=$1
+
+if [ ${PARAM_SKIP_DL} = "-s" ]; then
+    echo "  Skipping download, using previous download file."
+
+    if [ ! -f ./data ]; then
+        echo "  Previous download file not present. Cannot continue."
+        exit 1
+    fi
+
+    if [ ! $(cat ./data | jq > /dev/null 2> /dev/null ) ]; then
+        echo "  Previous download file present but not valid JSON. Cannot continue."
+        exit 1
+    fi
+else
+    echo "  Downloading fresh data from OpenStreetMap Overpass API."
+    bash download_data.sh
+fi
+
+# Whether we're using the old data or new downloaded data, transform it.
+echo "  Transforming data with Node script."
+node transform_data.js
+
 IMAGE="mwelke/overpass-api-bicycle-parking-importer"
 TAG_DATE="$(date +%Y%m%d)"
 
-# Include download_data
-bash download_data.sh
-
+echo "  Docker image tags will be:"
+echo "    ${IMAGE}:${TAG_DATE}"
+echo "    ${IMAGE}:latest"
 docker build -t ${IMAGE}:${TAG_DATE} .
+echo "  Docker image built."
 
 # Additional tags
 docker tag ${IMAGE}:${TAG_DATE} ${IMAGE}:latest
--- download_data.sh
+++ download_data.sh
@@ -1,11 +1,5 @@
 # Download latest OSM data, map it to just the elements array, replacing "id: " with "_id: "
-echo "Downloading OSM data. Please wait. This can take up to 3 minutes..."
+echo "  Downloading OSM data. This can take a few minutes."
 
 curl 'https://lz4.overpass-api.de/api/interpreter?data=\[out:json\]\[timeout:180\];(node\[amenity=bicycle_parking\];);out;' \
   | jq '.elements' > data
-
-# Use Node script to transform JSON data
-node transform_data.js
-
-# Delete old data
-rm data
