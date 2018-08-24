d95be0c03c64618ca692788d26d40185c06e38ca
Matt Welke <mattwelke@gmail.com>
2018-08-23 21:20:01 -0400

    Change URL to load balancer.

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,5 @@
 # Download latest OSM data, map it to just the elements array, replacing "id: " with "_id: "
 echo "  Downloading OSM data. This can take a few minutes."
 
-curl 'https://lz4.overpass-api.de/api/interpreter?data=\[out:json\]\[timeout:180\];(node\[amenity=bicycle_parking\];);out;' \
+curl 'https://overpass-api.de/api/interpreter?data=\[out:json\]\[timeout:180\];(node\[amenity=bicycle_parking\];);out;' \
   | jq '.elements' > data
